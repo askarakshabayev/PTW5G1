20db578427f52d346426e6af2406dc6215b5074f
unknown <a.akshabaev@c441a-10.kbtu.kz>
2017-02-13 11:29:44 +0600

    pt w5 snake

---
+++ W5Snake/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W5Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Snake snake = new Snake();
+            Wall wall = new Wall();
+            while (true)
+            {
+                ConsoleKeyInfo pressed = Console.ReadKey();
+                if (pressed.Key == ConsoleKey.UpArrow)
+                    snake.Move(0, -1);
+                if (pressed.Key == ConsoleKey.DownArrow)
+                    snake.Move(0, 1);
+                if (pressed.Key == ConsoleKey.LeftArrow)
+                    snake.Move(-1, 0);
+                if (pressed.Key == ConsoleKey.RightArrow)
+                    snake.Move(1, 0);
+                if (pressed.Key == ConsoleKey.Escape)
+                    break;
+                Console.Clear();
+                snake.Draw();
+                wall.Draw();
+            }
+        }
+    }
+}
